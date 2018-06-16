0c89bd49ac2430dc041976238cd5013b4f306f0b
Igor Nesterenko <igornester@gmail.com>
2018-06-16 16:55:40 +0300

    - add default pagination

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import {API_DEFAULT_PAGGINATION_OPTIONS} from "../constants";
 
-export const fetchSpecLookups = async () => {
-    const response = await axios.get('/specs');
+export const fetchSpecLookups = async (options = API_DEFAULT_PAGGINATION_OPTIONS) => {
+    const response = await axios.get('/specs', { params : options});
     return response.data;
 };
 
