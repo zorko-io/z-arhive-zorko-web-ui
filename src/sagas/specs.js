d85a3f8aa0cf467a0f6ef92c548931cdfc8b38d3
Igor Nesterenko <igornester@gmail.com>
2018-06-16 18:24:27 +0300

    - make request

--- src/sagas/specs.js
+++ src/sagas/specs.js
@@ -1,5 +1,17 @@
-export default function watchSpecs() {
+import {takeLatest, call} from 'redux-saga/effects';
+import * as Api from "../api";
+import {SPEC_LOOKUPS_REQUEST} from "../action";
 
+export function* fetchSpecLookup() {
+    try {
+        const specLookups = yield call(Api.fetchSpecLookups);
+    } catch (e) {
+        console.error(e);
+    }
+}
+
+export default function* watchSpecs() {
+  yield takeLatest(SPEC_LOOKUPS_REQUEST, fetchSpecLookup)
 
 
 }
