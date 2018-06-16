7ba4d4dc47189268ec292b2606de463523b07075
Igor Nesterenko <igornester@gmail.com>
2018-06-16 17:30:51 +0300

    - enable redux dev tools

--- src/store/index.js
+++ src/store/index.js
@@ -1,7 +1,20 @@
-import {createStore} from 'redux';
+import {createStore, compose, applyMiddleware} from 'redux';
 import {DEFAULT_APP_STATE} from "../state";
 import rootReducer from '../reducer';
 
+let composeEnhancers = compose;
+const reduxDevToolsEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__;
+
+if (process.env.NODE_ENV === 'development' && reduxDevToolsEnhancer) {
+    composeEnhancers = reduxDevToolsEnhancer;
+}
+
+const middleware = [];
+
+
 export default function createAppStore () {
-    return createStore(rootReducer, { ...DEFAULT_APP_STATE})
+    const enhancer = composeEnhancers(applyMiddleware(...middleware));
+
+    let initialState = { ...DEFAULT_APP_STATE};
+    return createStore(rootReducer, initialState, enhancer)
 };
