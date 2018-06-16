6a5dcce577adddde330a1ab0668336ae3cdfac79
Igor Nesterenko <igornester@gmail.com>
2018-06-16 17:04:54 +0300

    - re-export for api

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import * as Api from './api/index';
+import * as Api from './api';
 
 class App extends Component {
 
--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
-import {API_DEFAULT_PAGGINATION_OPTIONS} from "../constants";
 
-export const fetchSpecLookups = async (options = API_DEFAULT_PAGGINATION_OPTIONS) => {
-    const response = await axios.get('/specs', { params : options});
-    return response.data;
-};
 
+export * from './specs';
 
 axios.defaults.baseURL = process.env.REACT_APP_ZORKO_SERVER_BASE_URL;
