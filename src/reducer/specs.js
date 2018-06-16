c20a1c94d40eaf082c4df992060b4927f7cb413d
Igor Nesterenko <igornester@gmail.com>
2018-06-16 19:01:22 +0300

    - add spec previews to store

--- src/reducer/specs.js
+++ src/reducer/specs.js
@@ -1,9 +1,15 @@
 import {DEFAULT_SPECS_STATE} from "../state";
+import {SPEC_LOOKUPS_SET} from "../action";
 
 const initialState = {...DEFAULT_SPECS_STATE};
 
 export default function specsReducer(state = initialState, action) {
     switch (action.type){
+        case (SPEC_LOOKUPS_SET):
+            return {
+                ...state,
+                previews: action.payload
+            };
         default:
             return state
     }
