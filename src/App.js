f6e8a95893418380b8ab61e60e7fbe3e474e5081
Igor Nesterenko <igornester@gmail.com>
2018-06-18 23:17:59 +0300

    - getting started with navbar

--- src/App.js
+++ src/App.js
@@ -18,19 +18,26 @@ class App extends Component {
 
     return (
         <Fragment>
-            <nav class="navbar" role="navigation" aria-label="main navigation">
+            <nav className="navbar" aria-label="main navigation">
                 <div className="navbar-brand">
                     <a className="navbar-item" href="https://www.zorko.io">
                         <img src={logo}
                              alt="Zorko: a place to discover and share visualizations"/>
                     </a>
-
-                    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false">
-                        <span aria-hidden="true"/>
-                        <span aria-hidden="true"/>
-                        <span aria-hidden="true"/>
-                    </a>
+                    <div className="navbar-burger burger" data-target="navMenuTarget">
+                        <span/>
+                        <span/>
+                        <span/>
+                    </div>
                 </div>
+                <div id="navMenuTarget" className="navbar-menu">
+                   <div className="navbar-end">
+                       <div className="navbar-item">
+                           <button className="button">Sing in</button>
+                       </div>
+                   </div>
+                </div>
+
             </nav>
             <main>
                 {previews.map((specPreview, i)=>(
