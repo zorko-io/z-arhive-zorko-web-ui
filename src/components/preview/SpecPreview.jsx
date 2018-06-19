e6b47f28631e539cfa6621a7c50142f95da396ab
Igor Nesterenko <igornester@gmail.com>
2018-06-19 22:53:54 +0300

    - provide the same dimentions for all previews

--- src/components/preview/SpecPreview.jsx
+++ src/components/preview/SpecPreview.jsx
@@ -3,11 +3,25 @@ import PropTypes from "prop-types";
 
 class SpecPreview extends Component {
 
+    constructor (props) {
+        super(props);
+        this.svgParent = React.createRef();
+    }
+
+    componentDidMount () {
+        const nodes = Array.from(this.svgParent.current.children);
+        const svgPreview = nodes.pop();
+        svgPreview.setAttribute('width', '280');
+        svgPreview.setAttribute('height', '158');
+    }
+
     render() {
         const { content } = this.props;
 
         return (
-            <div  dangerouslySetInnerHTML={ {__html: content }}/>
+            <div ref={this.svgParent}
+                 dangerouslySetInnerHTML={ {__html: content }}
+            />
 
         );
     }
