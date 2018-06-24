0de4cfd1800ebeed1f32bab719f9fb675baabe20
Igor Nesterenko <igornester@gmail.com>
2018-06-24 10:08:42 +0300

    - split previews by columns

--- src/components/preview/SpecPreviewBlock.jsx
+++ src/components/preview/SpecPreviewBlock.jsx
@@ -2,29 +2,61 @@ import React, {Component, Fragment} from "react";
 import PropTypes from "prop-types";
 import {getAllPreviews} from "../../selector";
 import {connect} from "react-redux";
+import * as R from 'ramda';
 import SpecPreview from "./SpecPreview";
 
 class SpecPreviewBlock extends Component {
 
     render() {
-        const { previews } = this.props;
+        const columns = this.columns;
 
         return (
            <Fragment>
-
                <div className="container">
                    <div className="columns">
-                       <div className="column">
-                           {previews.map((specPreview, i)=>(
-                               <SpecPreview key={i} content={specPreview.preview}/>
-                           ))}
-                       </div>
+                       {columns.map((rows, i)=> (
+                           <div key={i} className="column">
+                               {rows.map((spec, j) => (
+                                   <SpecPreview key={`${i}-${j}`} content={spec.preview}/>
+                               ))}
+                           </div>
+                       ))}
                    </div>
                </div>
-
            </Fragment>
         );
     }
+
+    get columns () {
+        const { previews } = this.props;
+
+        const length = previews.length;
+        const ITEMS_IN_ROW = 3;
+        const itemsInLastRow = length % ITEMS_IN_ROW;
+        const rowsCount = (length - itemsInLastRow) / ITEMS_IN_ROW + (itemsInLastRow ? 1 : 0);
+
+        return R.range(1, rowsCount + 1).reduce((memo, rowIndex) => {
+            let previewChunkStartIndex = rowIndex > 0 ? (rowIndex - 1) * ITEMS_IN_ROW : 0;
+            let previewChunkEndRowIndex = rowIndex > 0 ? rowIndex * ITEMS_IN_ROW : ITEMS_IN_ROW;
+            if (rowIndex === rowsCount) {
+                previewChunkEndRowIndex += itemsInLastRow;
+            }
+            let chunk = previews.slice(previewChunkStartIndex, previewChunkEndRowIndex);
+
+            if (chunk.length > 0){
+                memo[0].push(chunk[0]);
+            }
+            if (chunk.length > 1){
+                memo[1].push(chunk[1]);
+            }
+            if (chunk.length > 2){
+                memo[2].push(chunk[2]);
+            }
+
+            return memo;
+        }, [ [], [], []]);
+    }
+
 }
 
 SpecPreviewBlock.propTypes = {
