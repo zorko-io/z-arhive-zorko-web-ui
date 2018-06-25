f99934cfe3f58f7c630b5d925167a80e58f1cce3
Igor Nesterenko <igornester@gmail.com>
2018-06-25 19:52:43 +0300

    - include router reducer

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux'
 import previews from './previews'
 import specs from './specs'
 import error from './error'
+import { routerReducer } from 'react-router-redux'
 
 const rootReducer = combineReducers({
   specs,
   previews,
-  error
+  error,
+  router: routerReducer
 })
 
 export default rootReducer
