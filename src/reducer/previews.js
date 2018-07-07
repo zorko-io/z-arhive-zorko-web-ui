9c393e4cd182a4cf3cf94cbd5dcd9239f06a98af
Igor Nesterenko <igornester@gmail.com>
2018-07-07 23:28:03 +0300

    - add reducer to calculate offset and limitˆ

--- src/reducer/previews.js
+++ src/reducer/previews.js
@@ -1,10 +1,28 @@
 import { DEFAULT_PREVIEWS_STATE } from '../state'
 import { SPEC_LOOKUPS_SET } from '../action'
+import { HOME_PAGE_INIT } from '../action/home'
 
 const initialState = { ...DEFAULT_PREVIEWS_STATE }
 
 export default function previewReducer(state = initialState, action) {
   switch (action.type) {
+    case HOME_PAGE_INIT:
+      const {
+        match: { params }
+      } = action.payload
+      let offset = 0
+      const limit = 6
+
+      if (params) {
+        const pageNumber = params.specId
+        offset = pageNumber * limit
+      }
+
+      return {
+        ...state,
+        limit,
+        offset
+      }
     case SPEC_LOOKUPS_SET:
       const previews = action.payload
       return {
