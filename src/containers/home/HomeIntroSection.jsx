794e5c5be79117d7a7dcac99548b76e3ab916a88
Igor Nesterenko <igornester@gmail.com>
2018-08-04 23:04:51 +0300

    - provide url to typeform

--- src/containers/home/HomeIntroSection.jsx
+++ src/containers/home/HomeIntroSection.jsx
@@ -1,5 +1,10 @@
 import React, { Component } from 'react'
 
+let feedbackUrl
+if (typeof process.env.REACT_APP_ZORKO_FEEDBACK_FORM !== 'undefined') {
+  feedbackUrl = process.env.REACT_APP_ZORKO_FEEDBACK_FORM
+}
+
 class HomeIntroSection extends Component {
   render() {
     return (
@@ -12,7 +17,11 @@ class HomeIntroSection extends Component {
             <h2 className="subtitle">
               Charts, graphincs and maps build on top of public data (json,csv) in the web
             </h2>
-            <button className="button is-primary">Contact us</button>
+            {feedbackUrl && (
+              <a className="button is-primary" href={feedbackUrl}>
+                Contact us
+              </a>
+            )}
           </div>
         </div>
       </section>
